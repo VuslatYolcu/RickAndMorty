935d7b311384694fccea9a38ddd7402a87f54cc7
VuslatYolcu <ylcvalt@gmail.com>
2023-04-17 19:42:49 +0300

    Create API Call structure

---
+++ RickAndMorty/Models/APIResponseTypes/RMGetAllCharactersResponse.swift
@@ -0,0 +1,22 @@
+//
+//  RMGetAllCharactersResponse.swift
+//  RickAndMorty
+//
+//  Created by Vuslat Yolcu on 17.04.2023.
+//
+
+import Foundation
+
+struct RMGetAllCharactersResponse: Codable {
+    
+    struct Info: Codable {
+        let count: Int
+        let pages: Int
+        let next: String?
+        let prev: String?
+    }
+    
+    let info: Info
+    let results: [RMCharacter]
+}
+
