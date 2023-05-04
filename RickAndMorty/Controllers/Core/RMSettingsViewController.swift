d50c84928aaa36c125a6c403c69bcc2118bb13b4
VuslatYolcu <ylcvalt@gmail.com>
2023-05-04 16:19:00 +0300

    Create Settings view model

--- RickAndMorty/Controllers/Core/RMSettingsViewController.swift
+++ RickAndMorty/Controllers/Core/RMSettingsViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 /// Controller to show and search for settings
 final class RMSettingsViewController: UIViewController {
 
+    private let viewModel = RMSettingsViewViewModel(cellViewModels: RMSettingsOption.allCases.compactMap({
+        return RMSettingsCellViewModel(type: $0)
+    }))
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
---
+++ RickAndMorty/ViewModels/Settings/RMSettingsViewViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  RMSettingsViewViewModel.swift
+//  RickAndMorty
+//
+//  Created by Vuslat Yolcu on 4.05.2023.
+//
+
+import Foundation
+
+struct RMSettingsViewViewModel {
+    let cellViewModels: [RMSettingsCellViewModel]
+
+}
