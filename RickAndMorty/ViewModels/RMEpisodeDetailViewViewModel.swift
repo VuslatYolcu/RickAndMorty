707e9852d08a236e06329a539c62b420c388bcdc
VuslatYolcu <ylcvalt@gmail.com>
2023-05-04 09:22:11 +0300

    Create episode detail layout

--- RickAndMorty/ViewModels/RMEpisodeDetailViewViewModel.swift
+++ RickAndMorty/ViewModels/RMEpisodeDetailViewViewModel.swift
@@ -20,7 +20,14 @@ final class RMEpisodeDetailViewViewModel {
         }
     }
     
+    enum SectionType {
+        case information(viewModels: [RMEpisodeInfoCollectionViewCellViewModel])
+        case characters(viewModel: [RMCharacterCollectionViewCellViewModel])
+    }
+    
     public weak var delegate: RMEpisodeDetailViewViewModelDelegate?
+    
+    public private(set) var section: [SectionType] = []
     
     // MARK: - Init
     init(endpointUrl: URL?) {
---
+++ RickAndMorty/Views/EpisodeDetails/RMEpisodeInfoCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  RMEpisodeInfoCollectionViewCell.swift
+//  RickAndMorty
+//
+//  Created by Vuslat Yolcu on 4.05.2023.
+//
+
+import UIKit
+
+final class RMEpisodeInfoCollectionViewCell: UICollectionViewCell {
+    
+}
